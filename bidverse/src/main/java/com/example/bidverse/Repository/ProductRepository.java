b9dd965efc1aafdd2529394316113b43c46dee1a
Samakas <samakash1245@gmail.com>
2026-09-02 10:35:22 +0530

    Sam

---
+++ bidverse/src/main/java/com/example/bidverse/Repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.bidverse.Repository;
+
+import com.example.bidverse.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByStatus(String status);
+
+    Product findById(Long productId);
+
+    void save(Product product);
+}
