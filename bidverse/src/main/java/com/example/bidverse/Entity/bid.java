401287437e34762325d07d63659ad50995b15abc
Samakas <samakash1245@gmail.com>
2026-09-08 22:24:57 +0530

    Added Authentication, Allowed user to join before room completes and few api to fetch history and deals

--- bidverse/src/main/java/com/example/bidverse/Entity/bid.java
+++ bidverse/src/main/java/com/example/bidverse/Entity/bid.java
@@ -13,10 +13,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "bids", uniqueConstraints = @UniqueConstraint(
-        name = "uq_bids_auction_item_buyer",
-        columnNames = {"auction_item_id", "buyer_id"}
-))
+@Table(name = "bids")
 public class bid {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
